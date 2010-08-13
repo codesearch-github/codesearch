c1d92bb32624ad476da04e4bf02470211d971ea3
Samuel Kogler <samuel.kogler@gmail.com>
2010-08-13 18:12:11 +0200

    Module manager(WIP),

---
+++ src/commons/src/main/java/org/codesearch/commons/util/ModuleManager.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2010 David Froehlich   <david.froehlich@businesssoftware.at>,
+ *                Samuel Kogler     <samuel.kogler@gmail.com>,
+ *                Stephan Stiboller <stistc06@htlkaindorf.at>
+ *
+ * This file is part of Codesearch.
+ *
+ * Codesearch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Codesearch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codesearch.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.codesearch.commons.util;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.codesearch.commons.configreader.xml.PropertyManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Used to parse the configuration for a module.
+ * @author Samuel Kogler
+ */
+public class ModuleManager {
+
+    /** The filename of the default module configuration. */
+    private static final String DEFAULT_CONFIG_FILENAME = "module-config.xml";
+    /** The property manager used to read the configuration. */
+    @Autowired
+    private PropertyManager propertyManager;
+    /** The initialized application context. */
+    private ApplicationContext applicationContext;
+
+    /**
+     * Returns the locations of the spring configuration files
+     * defined in the module configuration.
+     * @return The locations of the spring configuration files.
+     */
+    public List<String> getSpringConfigLocations() {
+        List<String> springConfigLocations = new LinkedList<String>();
+        propertyManager.setConfigpath(DEFAULT_CONFIG_FILENAME);
+        //springConfigLocations.addAll(propertyManager.getSingleLinePropertyValueList("springConfigLocation"));
+        return springConfigLocations;
+    }
+
+}
---
+++ src/commons/src/test/java/org/codesearch/commons/util/ModuleManagerTest.java
@@ -0,0 +1,43 @@
+
+package org.codesearch.commons.util;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests the module manager.
+ * @author Samuel Kogler
+ */
+public class ModuleManagerTest {
+
+    public ModuleManagerTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getSpringConfigLocations method, of class ModuleManager.
+     */
+    @Test
+    public void testGetSpringConfigLocations() {
+        System.out.println("getSpringConfigLocations");
+        ModuleManager instance = new ModuleManager();
+        List expResult = null;
+        List result = instance.getSpringConfigLocations();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
