c46a872c6205a27bdfda87b08867e56b1381ab19
skogler <samuel.kogler@gmail.com>
2012-02-14 12:45:01 +0100

    commons: add configuration validator

--- src/commons/src/main/java/org/codesearch/commons/CommonsGuiceModule.java
+++ src/commons/src/main/java/org/codesearch/commons/CommonsGuiceModule.java
@@ -1,30 +1,29 @@
 /**
- * Copyright 2010 David Froehlich   <david.froehlich@businesssoftware.at>,
- *                Samuel Kogler     <samuel.kogler@gmail.com>,
- *                Stephan Stiboller <stistc06@htlkaindorf.at>
+ * Copyright 2010 David Froehlich <david.froehlich@businesssoftware.at>, Samuel
+ * Kogler <samuel.kogler@gmail.com>, Stephan Stiboller <stistc06@htlkaindorf.at>
  *
  * This file is part of Codesearch.
  *
- * Codesearch is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Codesearch is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * Codesearch is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Codesearch is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Codesearch.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * Codesearch. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.codesearch.commons;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
-import com.google.inject.name.Names;
+import org.apache.log4j.Logger;
 import org.codesearch.commons.configuration.ConfigurationReader;
+import org.codesearch.commons.configuration.ConfigurationValidator;
+import org.codesearch.commons.configuration.InvalidConfigurationException;
 import org.codesearch.commons.configuration.properties.PropertiesManager;
 import org.codesearch.commons.configuration.properties.RepositoryRevisionManager;
 import org.codesearch.commons.configuration.xml.XmlConfigurationReader;
@@ -39,18 +38,28 @@ import org.codesearch.commons.plugins.lucenefields.LuceneFieldPluginLoaderImpl;
 
 /**
  * Guice module for commons. Wires everything together.
+ *
  * @author Samuel Kogler
  */
-public class CommonsGuiceModule extends AbstractModule{
+public class CommonsGuiceModule extends AbstractModule {
+
+    private final Logger LOG = Logger.getLogger(CommonsGuiceModule.class);
 
     @Override
     protected void configure() {
-        bind(ConfigurationReader.class).to(XmlConfigurationReader.class).in(Singleton.class);
-        bind(PropertiesManager.class).to(RepositoryRevisionManager.class).in(Singleton.class);
-        bind(ConnectionPool.class).to(ConnectionPoolImpl.class).in(Singleton.class);
-        bind(DBAccess.class).to(DBAccessImpl.class).in(Singleton.class);
-        bind(PluginLoader.class).to(PluginLoaderImpl.class).in(Singleton.class);
-        bind(LuceneFieldPluginLoader.class).to(LuceneFieldPluginLoaderImpl.class).in(Singleton.class);
-        bind(String.class).annotatedWith(Names.named("configpath")).toInstance("codesearch_config.xml");
+        try {
+            ConfigurationReader configurationReader = new XmlConfigurationReader("codesearch_config.xml");
+            PluginLoader pluginLoader = new PluginLoaderImpl(configurationReader);
+            ConfigurationValidator configurationValidator = new ConfigurationValidator(configurationReader, pluginLoader);
+
+            bind(ConfigurationReader.class).toInstance(configurationReader);
+            bind(PluginLoader.class).toInstance(pluginLoader);
+            bind(PropertiesManager.class).to(RepositoryRevisionManager.class).in(Singleton.class);
+            bind(ConnectionPool.class).to(ConnectionPoolImpl.class).in(Singleton.class);
+            bind(DBAccess.class).to(DBAccessImpl.class).in(Singleton.class);
+            bind(LuceneFieldPluginLoader.class).to(LuceneFieldPluginLoaderImpl.class).in(Singleton.class);
+        } catch (InvalidConfigurationException ex) {
+            LOG.error("Invalid configuration:", ex);
+        }
     }
 }
---
+++ src/commons/src/test/java/org/codesearch/commons/configuration/xml/ConfigurationValidatorTest.java
@@ -0,0 +1,133 @@
+/*
+ *
+ *
+ */
+package org.codesearch.commons.configuration.xml;
+
+import java.io.File;
+import org.codesearch.commons.plugins.NullPluginLoader;
+import org.codesearch.commons.configuration.ConfigurationReader;
+import org.codesearch.commons.configuration.ConfigurationValidator;
+import org.codesearch.commons.configuration.InvalidConfigurationException;
+import org.codesearch.commons.configuration.dto.RepositoryDto;
+import org.codesearch.commons.plugins.ExceptionThrowingPluginLoader;
+import org.codesearch.commons.plugins.PluginLoader;
+import org.junit.Test;
+
+/**
+ *
+ * @author Samuel Kogler
+ */
+public class ConfigurationValidatorTest {
+
+    @Test
+    public void testInvalidConfiguration() {
+        //assert that the error handling prevents loading an incomplete repository configuraion
+        try {
+            ConfigurationReader configReader = new XmlConfigurationReader("codesearch_config_invalid_repository_configuration.xml");
+            PluginLoader pluginLoader = new NullPluginLoader();
+            ConfigurationValidator configurationValidator = new ConfigurationValidator(configReader, pluginLoader);
+            assert (false);
+        } catch (InvalidConfigurationException ex) {
+            //in case the exception is thrown everything is fine
+        }
+    }
+
+    @Test
+    public void testInvalidDirectories() {
+        //assert that the error handling prevents loading an incomplete repository configuraion
+        try {
+            MockConfigurationReader configReader = new MockConfigurationReader();
+            configReader.configuration.setCacheDirectory(new File("/asdf"));
+            configReader.configuration.setIndexLocation(new File("/sadfsad"));
+
+            PluginLoader pluginLoader = new NullPluginLoader();
+            ConfigurationValidator configurationValidator = new ConfigurationValidator(configReader, pluginLoader);
+            assert (false);
+        } catch (InvalidConfigurationException ex) {
+            //in case the exception is thrown everything is fine
+        }
+    }
+
+    @Test
+    public void testUnnamedRepo() {
+        //assert that the error handling prevents loading an incomplete repository configuraion
+        try {
+            MockConfigurationReader configReader = new MockConfigurationReader();
+            configReader.configuration.setCacheDirectory(new File("/tmp"));
+            configReader.configuration.setIndexLocation(new File("/tmp"));
+            RepositoryDto repo = new RepositoryDto();
+            repo.setUrl("sdf");
+            repo.setVersionControlSystem("TESTVCS");
+            configReader.configuration.getRepositories().add(repo);
+
+            PluginLoader pluginLoader = new NullPluginLoader();
+            ConfigurationValidator configurationValidator = new ConfigurationValidator(configReader, pluginLoader);
+            assert (false);
+        } catch (InvalidConfigurationException ex) {
+            //in case the exception is thrown everything is fine
+        }
+    }
+
+    @Test
+    public void testNoVcs() {
+        //assert that the error handling prevents loading an incomplete repository configuraion
+        try {
+            MockConfigurationReader configReader = new MockConfigurationReader();
+            configReader.configuration.setCacheDirectory(new File("/tmp"));
+            configReader.configuration.setIndexLocation(new File("/tmp"));
+            RepositoryDto repo = new RepositoryDto();
+            repo.setName("asdf");
+            repo.setUrl("sdf");
+            configReader.configuration.getRepositories().add(repo);
+
+            PluginLoader pluginLoader = new NullPluginLoader();
+            ConfigurationValidator configurationValidator = new ConfigurationValidator(configReader, pluginLoader);
+            assert (false);
+        } catch (InvalidConfigurationException ex) {
+            //in case the exception is thrown everything is fine
+        }
+    }
+
+    @Test
+    public void testInvalidVcs() {
+        //assert that the error handling prevents loading an incomplete repository configuraion
+        try {
+            MockConfigurationReader configReader = new MockConfigurationReader();
+            configReader.configuration.setCacheDirectory(new File("/tmp"));
+            configReader.configuration.setIndexLocation(new File("/tmp"));
+            RepositoryDto repo = new RepositoryDto();
+            repo.setName("asdf");
+            repo.setUrl("sdf");
+            repo.setVersionControlSystem("testvcs");
+            configReader.configuration.getRepositories().add(repo);
+
+            PluginLoader pluginLoader = new ExceptionThrowingPluginLoader();
+            ConfigurationValidator configurationValidator = new ConfigurationValidator(configReader, pluginLoader);
+            assert (false);
+        } catch (InvalidConfigurationException ex) {
+            //in case the exception is thrown everything is fine
+        }
+    }
+
+    @Test
+    public void testInvalidRegex() {
+        //assert that the error handling prevents loading an incomplete repository configuraion
+        try {
+            MockConfigurationReader configReader = new MockConfigurationReader();
+            configReader.configuration.setCacheDirectory(new File("/tmp"));
+            configReader.configuration.setIndexLocation(new File("/tmp"));
+            RepositoryDto repo = new RepositoryDto();
+            repo.setName("asdf");
+            repo.setUrl("sdf");
+            repo.setVersionControlSystem("testvcs");
+            repo.getBlacklistEntries().add("/(((");
+            configReader.configuration.getRepositories().add(repo);
+            PluginLoader pluginLoader = new NullPluginLoader();
+            ConfigurationValidator configurationValidator = new ConfigurationValidator(configReader, pluginLoader);
+            assert (false);
+        } catch (InvalidConfigurationException ex) {
+            //in case the exception is thrown everything is fine
+        }
+    }
+}
