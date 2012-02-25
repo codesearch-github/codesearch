f4bde186ed90b06d8fca4abfe26ed0f51f310d17
skogler <samuel.kogler@gmail.com>
2012-02-25 12:25:38 +0100

    indexer: update dashboard periodically

--- src/indexer/src/main/java/org/codesearch/indexer/client/ui/dashboard/DashboardActivity.java
+++ src/indexer/src/main/java/org/codesearch/indexer/client/ui/dashboard/DashboardActivity.java
@@ -1,30 +1,28 @@
 /**
- * Copyright 2010 David Froehlich   <david.froehlich@businesssoftware.at>,
- *                Samuel Kogler     <samuel.kogler@gmail.com>,
- *                Stephan Stiboller <stistc06@htlkaindorf.at>
+ * Copyright 2010 David Froehlich <david.froehlich@businesssoftware.at>, Samuel
+ * Kogler <samuel.kogler@gmail.com>, Stephan Stiboller <stistc06@htlkaindorf.at>
  *
  * This file is part of Codesearch.
  *
- * Codesearch is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Codesearch is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * Codesearch is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Codesearch is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Codesearch.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * Codesearch. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.codesearch.indexer.client.ui.dashboard;
 
 import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.place.shared.Place;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
@@ -39,6 +37,7 @@ import org.codesearch.indexer.shared.RepositoryStatus;
 
 /**
  * Presenter for the dashboard.
+ *
  * @author Samuel Kogler
  */
 public class DashboardActivity extends AbstractActivity implements Presenter {
@@ -46,16 +45,18 @@ public class DashboardActivity extends AbstractActivity implements Presenter {
     private ClientFactory clientFactory;
     private DashboardView dashboardView;
     private DashboardServiceAsync dashboardServiceAsync = GWT.create(DashboardService.class);
-
     private ListDataProvider<JobStatus> runningJobsDataProvider = new ListDataProvider<JobStatus>();
     private ListDataProvider<JobStatus> scheduledJobsDataProvider = new ListDataProvider<JobStatus>();
     private ListDataProvider<RepositoryStatus> repositoryStatuses = new ListDataProvider<RepositoryStatus>();
+    private Timer refreshTimer;
 
     public DashboardActivity(ClientFactory clientFactory, DashboardPlace place) {
         this.clientFactory = clientFactory;
     }
 
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void start(AcceptsOneWidget panel, EventBus eventBus) {
         dashboardView = clientFactory.getDashboardView();
@@ -66,9 +67,20 @@ public class DashboardActivity extends AbstractActivity implements Presenter {
         scheduledJobsDataProvider.addDataDisplay(dashboardView.getScheduledJobsTable());
         repositoryStatuses.addDataDisplay(dashboardView.getRepositoryStatuses());
         refresh();
+
+        refreshTimer = new Timer() {
+
+            @Override
+            public void run() {
+                refresh();
+            }
+        };
+        refreshTimer.scheduleRepeating(2000);
     }
 
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void goTo(Place place) {
         clientFactory.getPlaceController().goTo(place);
@@ -81,28 +93,33 @@ public class DashboardActivity extends AbstractActivity implements Presenter {
         runningJobsDataProvider.removeDataDisplay(dashboardView.getRunningJobsTable());
         scheduledJobsDataProvider.removeDataDisplay(dashboardView.getScheduledJobsTable());
         repositoryStatuses.removeDataDisplay(dashboardView.getRepositoryStatuses());
+        refreshTimer.cancel();
     }
 
     @Override
     public void refresh() {
-    	dashboardView.cleanup();
-    	dashboardServiceAsync.getData(new DashboardDataCallback());
+        dashboardView.cleanup();
+        dashboardServiceAsync.getData(new DashboardDataCallback());
     }
 
     class DashboardDataCallback implements AsyncCallback<DashboardData> {
 
-		/** {@inheritDoc} */
-		@Override
-		public void onFailure(Throwable caught) {
-			Window.alert("Error retrieving data for the dashboard: " + caught);
-		}
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onFailure(Throwable caught) {
+            Window.alert("Error retrieving data for the dashboard: " + caught);
+        }
 
-		/** {@inheritDoc} */
-		@Override
-		public void onSuccess(DashboardData result) {
-			runningJobsDataProvider.setList(result.getRunningJobs());
-			scheduledJobsDataProvider.setList(result.getScheduledJobs());
-                        repositoryStatuses.setList(result.getRepositoryStatuses());
-		}
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onSuccess(DashboardData result) {
+            runningJobsDataProvider.setList(result.getRunningJobs());
+            scheduledJobsDataProvider.setList(result.getScheduledJobs());
+            repositoryStatuses.setList(result.getRepositoryStatuses());
+        }
     }
 }
