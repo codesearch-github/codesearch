da9aa120ddf143afc97fa948fa9deb393dd99513
marmichael <Marco.Michael@ATGRZ4037049M.corp.kpmgconsulting.com>
2012-05-02 13:40:50 +0200

    use templates interface to avoid inline html

--- src/indexer/src/main/java/org/codesearch/indexer/client/ui/dashboard/ProgressBarCell.java
+++ src/indexer/src/main/java/org/codesearch/indexer/client/ui/dashboard/ProgressBarCell.java
@@ -23,10 +23,44 @@ package org.codesearch.indexer.client.ui.dashboard;
 
 import com.google.gwt.cell.client.NumberCell;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.safecss.shared.SafeStyles;
+import com.google.gwt.safehtml.client.SafeHtmlTemplates;
+import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 
 
 public class ProgressBarCell extends NumberCell {
+
+    class ProgressBarWidthSafeStyle implements SafeStyles {
+
+        private static final long serialVersionUID = 8047291965851160143L;
+        private final int value;
+
+        public ProgressBarWidthSafeStyle(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String asString() {
+            return "width:" + value + "px;";
+        }
+
+    }
+
+    interface ProgressBarCellTemplates extends SafeHtmlTemplates {
+
+        @Template(// @formatter:off
+              "<div id='codeSearchProgressBarBackground' style='{1}border: 1px solid;'>"
+            + "  <div id='codeSearchProgressBarValue' style='{2}background: lightblue;'>"
+            + "&nbsp;{0}"
+            + "  </div>"
+            + "</div>")
+        // @formatter:on
+        SafeHtml progressBarWithValue(SafeHtml message, SafeStyles progressbarWidth, SafeStyles progressbarValue);
+    }
+
+    private static final ProgressBarCellTemplates TEMPLATES = GWT.create(ProgressBarCellTemplates.class);
 
     private int progressbarWidth = 300;
 
@@ -36,14 +70,12 @@ public class ProgressBarCell extends NumberCell {
 
     @Override
     public void render(Context context, Number value, SafeHtmlBuilder sb) {
-        sb.appendHtmlConstant("<div id='codeSearchProgressBarBackground' style='width:" + progressbarWidth
-            + "px;border: 1px solid;'>");
-        sb.appendHtmlConstant("<div id='codeSearchProgressBarValue' style='width:"
-            + (int)(value.floatValue() * progressbarWidth) + "px;background: lightblue;'>&nbsp;");
-        sb.append((int)(value.floatValue() * 100));
-        sb.appendEscaped("%");
-        sb.appendHtmlConstant("</div>");
-        sb.appendHtmlConstant("</div>");
-        GWT.log(sb.toSafeHtml().asString());
+        SafeHtml message = SafeHtmlUtils.fromString(String.valueOf((int)(value.floatValue() * 100) + "%"));
+        ProgressBarWidthSafeStyle barWidth = new ProgressBarWidthSafeStyle(progressbarWidth);
+        ProgressBarWidthSafeStyle valueWidth = new ProgressBarWidthSafeStyle(
+            (int)(value.floatValue() * progressbarWidth));
+        SafeHtml safeHtml = TEMPLATES.progressBarWithValue(message, barWidth, valueWidth);
+        sb.append(safeHtml);
+        GWT.log(safeHtml.asString());
     }
 }
